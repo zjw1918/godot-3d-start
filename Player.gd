d99c02fe5a4b56388c7c5b806205834a2e424ddd
rick <zjw1918@sina.cn>
2024-12-06 01:00:52 +0800

    player and view complete

--- Player.gd
+++ Player.gd
@@ -5,6 +5,10 @@ extends CharacterBody3D
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 
+
+@onready var pivot: Node3D = $Pivot
+
+
 var target_velocity = Vector3.ZERO
 
 
@@ -23,3 +27,20 @@ func _physics_process(delta):
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
+		
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		# Setting the basis property will affect the rotation of the node.
+		pivot.basis = Basis.looking_at(direction)
+		
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
